3a50e017b7b94ad817bb3f7967e9704f46edc181
Ahmed Hamdy <ahmedhamdy20@gmail.com>
2025-08-20 22:41:01 +0300

    Modified Main Entry file and Module after create new Separated Modules Plus some new implemenation

--- api/src/api.module.ts
+++ api/src/api.module.ts
@@ -1,7 +1,18 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
+import { DatabaseModule } from './database/database.module';
+import { UserModule } from './user/user.module';
+import { SharedModule } from './shared/shared.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    SharedModule,
+    DatabaseModule,
+    UserModule,
+    AuthModule
+  ],
   controllers: [],
   providers: [],
 })
--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { ApiModule } from './api.module';
+import { ValidationPipe } from '@nestjs/common';
 
+/**
+ * Main NestJS Application bootstrap
+ */
 async function bootstrap() {
+
   const app = await NestFactory.create(ApiModule);
-  await app.listen(process.env.PORT ?? 3000);
+  // Apply the ValidationPipe globally
+  app.useGlobalPipes(new ValidationPipe());
+
+  await app.listen(process.env.PORT ?? 3000, process.env.HOST ?? '127.0.0.1');
 }
+
 bootstrap();
