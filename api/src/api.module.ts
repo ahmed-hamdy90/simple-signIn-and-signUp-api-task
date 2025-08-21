029dbd6daff6138726fb182cc7847ec4685df445
Ahmed Hamdy <ahmedhamdy20@gmail.com>
2025-08-21 22:01:36 +0300

    Enhancement on Main/Entry API Module

--- api/src/api.module.ts
+++ api/src/api.module.ts
@@ -1,15 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { DatabaseModule } from './database/database.module';
-import { UserModule } from './user/user.module';
 import { SharedModule } from './shared/shared.module';
-import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    SharedModule,
     DatabaseModule,
+    SharedModule,
     UserModule,
     AuthModule
   ],
