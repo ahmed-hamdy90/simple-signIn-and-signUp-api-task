c2e1298b71878c9ae9a3dd821a19c921e47dfac1
Ahmed Hamdy <ahmedhamdy20@gmail.com>
2025-08-22 06:54:06 +0300

    Fix problem of Calling API after Enable CORS

--- api/src/main.ts
+++ api/src/main.ts
@@ -11,6 +11,10 @@ async function bootstrap() {
   // Apply the ValidationPipe globally
   app.useGlobalPipes(new ValidationPipe());
 
+  // Enable all CORS to Able Simple HTML to access API
+  // TODO: Make more Security to define only needed URLs - check if can load them from Configuration files
+  app.enableCors();
+
   await app.listen(process.env.PORT ?? 3000, process.env.HOST ?? '127.0.0.1');
 }
 
